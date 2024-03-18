310b8b6281fa76b47779d8eb76c3074a02e87cba
ilias-anouar <115695877+ilias-anouar@users.noreply.github.com>
2024-03-18 11:46:17 +0000

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -49,8 +49,8 @@ module.exports = {
       name: '@electron-forge/publisher-github',
       config: {
         repository: {
-          owner: 'github-user-name',
-          name: 'github-repo-name'
+          owner: 'ilias-anouar',
+          name: 'my-electron-app'
         },
         prerelease: false,
         draft: true
